f24506fdd7e676f81025b3316987df8d2aea5ed5
Pernille <pernille.wangsholm@gmail.com>
2016-08-04 08:49:35 +0200

    fixed testing

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,6 +2,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { assets } from 'meteor/practicalmeteor:chai';
 
 import { Tasks } from './tasks.js';
 
@@ -13,8 +14,29 @@ if (Meteor.isServer) {
 
 			beforeEach(() => {
 				Tasks.remove({});
-			})
+				taskId = Tasks.insert({
+					text = 'test task',
+					createdAt: new Date(),
+					owner: userId,
+					username: 'tmesaday',
+				});
+			});
+
 			it('can delete owned task', () => {
+
+				// Find the internal implementation of the task method so we can 
+				// test it in isolation
+
+				const deleteTask = Meteor.server.methods_handlers['task.remove'];
+
+				// Set up a fake method invocation that looks like what the method expects
+				const invocation = {userId};
+
+				// Run the method with 'this' set to the fake invocation
+				deleteTask.apply(invocation, [taskId]);
+
+				// Verify that the method does what we expected
+				assets.equal(Task.find().count(), 0);
 			});
 		});
 	});
